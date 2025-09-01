4c58f37cfc61683f3d66e7762fe74e8fbe63a189
hbj1572 <kimhanbj@naver.com>
2025-09-01 17:36:10 +0900

    Create 2739.cpp

---
+++ bronze/2739.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n1;
+    cin >> n1;
+    
+    for(int i = n1; i <= n1; i++){
+        for (int j = 1; j <= 9; j++){
+            cout << n1 << " * " << j << " = " << n1 * j << endl;
+        }
+    }
+    return 0;
+}

fe19be4fc6f802b7a2181e9916e5bdcbdf00cc05
hbj1572 <kimhanbj@naver.com>
2025-09-01 17:32:54 +0900

    Create 2577.cpp

---
+++ bronze/2577.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b, c;
+
+    int arr[10] = {0};
+
+    cin >> a >> b >> c;
+    int num = a * b * c;
+
+
+    while (num != 0){
+        arr[num % 10] += 1;
+        num /= 10;
+    }
+
+    for (int i = 0; i < 10; i++){
+        cout << arr[i] << '\n';
+    }
+    return 0;
+}

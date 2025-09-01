d7e08aff0a57cfd1bfc10dd36a0d54fc8289ad2b
hbj1572 <kimhanbj@naver.com>
2025-09-01 17:25:55 +0900

    Create 2475.cpp

---
+++ bronze/2475.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num, answer;
+    int square = 0;
+
+    for (int i = 0; i < 5; i++){
+        cin >> num;
+        square += num * num;
+    }
+
+    answer = square % 10;
+
+    cout << answer;
+    return 0;
+}

7c79dc3c6e508ba2814256a1c951eec349c10c35
hbj1572 <kimhanbj@naver.com>
2025-09-01 17:35:01 +0900

    Create 2609.cpp

---
+++ bronze/2609.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n1 = 0, n2 = 0;
+    cin >> n1 >> n2;
+    
+    cout << gcd(n1, n2) << '\n' << lcm(n1, n2);
+    
+    return 0;
+}

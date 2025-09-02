0a3b9d3560e9202f66b58d9526a5158e6579e74e
hbj1572 <kimhanbj@naver.com>
2025-09-02 14:57:59 +0900

    Create 10808.cpp

---
+++ bronze/10808.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    for (char a = 'a'; a <= 'z'; a++){
+        int cnt = 0;
+        for (auto c : s)
+            if (a == c) cnt++;
+            cout << cnt << ' ';  
+    }
+}

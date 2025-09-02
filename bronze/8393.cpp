074cd45d10995baf365081176432386b63a9f68f
hbj1572 <kimhanbj@naver.com>
2025-09-02 14:50:22 +0900

    Create 8393.cpp

---
+++ bronze/8393.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int sum = 0;
+    int n;
+    int i = 0;
+    cin >> n;
+
+    while (n >= i){
+        sum += i;
+        i++;
+        
+    }
+    cout << sum << endl;
+
+}

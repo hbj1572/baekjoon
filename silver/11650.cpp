88ac29be20ffaa6095445e726e8ced937bded416
hbj1572 <kimhanbj@naver.com>
2025-09-13 21:08:02 +0900

    Create 11650.cpp

---
+++ silver/11650.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector <pair<int, int>> v(n);
+
+    for (int i = 0; i < n; i++){
+        cin >> v[i].first >> v[i].second;
+    }
+
+    sort(v.begin(), v.end());
+
+    for (auto [x, y] : v){
+        cout << x << " " << y << '\n';
+    }
+    return 0;
+}
